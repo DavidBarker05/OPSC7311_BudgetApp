5005dc7d30ecfdb961f8df97130685bfd3112702
DavidBarker05 <davidbarker9611@gmail.com>
2026-09-07 15:18:34 +0200

    Added database stuff for user

---
+++ app/src/androidTest/java/com/example/mybudgettree/database/User.kt
@@ -0,0 +1,10 @@
+package com.example.mybudgettree.database
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "users")
+data class User(
+    @PrimaryKey val username: String,
+    val password: String
+)
---
+++ app/src/androidTest/java/com/example/mybudgettree/database/daos/UserDao.kt
@@ -0,0 +1,27 @@
+package com.example.mybudgettree.database.daos
+
+import com.example.mybudgettree.database.User
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Update
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.OnConflictStrategy
+
+@Dao
+interface UserDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insertUser(user: User): Long?
+
+    @Query("UPDATE users SET username = :newUsername WHERE username = :oldUsername")
+    fun updateUsername(oldUsername: String, newUsername: String)
+
+    @Query("UPDATE users SET password = :newPassword WHERE username = :username")
+    fun updatePassword(username: String, newPassword: String)
+
+    @Delete
+    fun deleteUser(user: User): Int
+
+    @Query("SELECT * FROM users WHERE username = :username")
+    fun findUser(username: String): User?
+}
